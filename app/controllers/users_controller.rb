06c778ac5b07118fa7c11558f78a64530f5bf01f
Min Ni <nimin98@gmail.com>
2012-01-30 21:41:32 -0800

    User model with passwords, and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
 
   def show
+    # communication to the model side
     @user  = User.find(params[:id])
+
+    # communication to the view side
+    @title = @user.name
   end
 
   def new
+    # communication to the model side
+
+    # communication to the view side
     @title = "Sign up"
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,18 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor   :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -22,5 +25,45 @@ class User < ActiveRecord::Base
   :presence   => true,
   :format     => { :with => email_regex },
   :uniqueness => { :case_sensitive => false }
+
+  validates :password, 
+  :presence     => true,
+  :confirmation => true,
+  :length       => { :within => 6..40 }
+
+  # register callback for "before_save" method, it will be called before database is
+  # saved; encrypt_password is the name of defined function.
+  before_save :encrypt_password
+
+  # return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+
+  def encrypt_password
+    # reset salt if it is the first time to create the password or password changes
+    self.salt = make_salt unless has_password?(password)
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
 
 end
